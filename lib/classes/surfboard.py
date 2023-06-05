12779fdb6ea8f8ff5073183ba20443149c7321a8
River Ferguson <riverferguson@gmail.com>
2023-06-04 19:35:34 -0700

    set up surfboard class

--- lib/classes/surfboard.py
+++ lib/classes/surfboard.py
@@ -1,1 +1,10 @@
 from .__init__ import CONN, CURSOR
+
+class Surfboard:
+    
+    def __init__(self, shaper, height, width, thickness, type):
+        self.shaper = shaper
+        self.height = height
+        self.width = width
+        self.thickness = thickness
+        self.type = type 
