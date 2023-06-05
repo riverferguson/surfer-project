3d68f1162e6a7f794239a43b4205f17fdd56c823
River Ferguson <riverferguson@gmail.com>
2023-06-04 19:48:21 -0700

    set up instances and properties for beaches class

--- lib/classes/beaches.py
+++ lib/classes/beaches.py
@@ -7,3 +7,38 @@ class Beaches:
         self.location = location
         self.popularity = popularity
         
+        
+    @property
+    def name(self):
+        return self._name
+    
+    @name.setter
+    def name(self, name):
+        if isinstance(name, str) and 1 <= len(name) <= 15:
+            self._name = name
+        else:
+            raise Exception('Name must be a string between 1 and 15 charcters!')
+        
+    @property
+    def location(self):
+        return self._location
+    
+    @location.setter
+    def location(self, location):
+        if isinstance(location, str) and 1 <= len(location) <= 20:
+            self._location = location
+        else:
+            raise Exception('Location must be between a string between 1 and 20 characters kook!')
+        
+    @property
+    def popularity(self):
+        return self._popularity
+    
+    @popularity.setter
+    def popularity(self, popularity):
+        if isinstance(popularity, int) and 1 <= popularity <= 10:
+            self._popularity = popularity
+        else:
+            raise Exception('Popularity must be a number between 1 and 10 bro!')
+        
+        
--- lib/classes/surfboard.py
+++ lib/classes/surfboard.py
@@ -2,9 +2,9 @@ from .__init__ import CONN, CURSOR
 
 class Surfboard:
     
-    def __init__(self, shaper, height, width, thickness, type):
+    def __init__(self, shaper, height, width, thickness, make):
         self.shaper = shaper
         self.height = height
         self.width = width
         self.thickness = thickness
-        self.type = type 
+        self.make = make 
