1ce7ded038c0eaa0b59ee5378cc4df0dfeed62a2
River Ferguson <riverferguson@gmail.com>
2023-06-04 19:33:43 -0700

    set up waves class

--- lib/classes/waves.py
+++ lib/classes/waves.py
@@ -1,1 +1,9 @@
 from .__init__ import CONN, CURSOR
+
+class Waves:
+    
+    def __init__(self, difficulty, local_attitude, danger_level):
+        self.difficulty = difficulty
+        self.local_attitude = local_attitude
+        self.danger_level = danger_level
+        
