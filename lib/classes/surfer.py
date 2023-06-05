1082f9f7e6e3efa9ec18241e225e13a2eb877e0c
River Ferguson <riverferguson@gmail.com>
2023-06-04 19:30:39 -0700

    set up surfer class

---
+++ lib/classes/surfer.py
@@ -0,0 +1,10 @@
+from .__init__ import CONN, CURSOR
+
+class Surfer:
+    
+    def __init__(self, first_name, last_name, age, motto):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+        self.motto = motto
+        
