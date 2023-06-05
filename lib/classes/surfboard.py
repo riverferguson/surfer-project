4049f1bc2a61abff688b6035b9c1f3d0156aa31d
River Ferguson <riverferguson@gmail.com>
2023-06-04 20:17:25 -0700

    create instances and properties of surboard class

--- lib/classes/surfboard.py
+++ lib/classes/surfboard.py
@@ -8,4 +8,37 @@ class Surfboard:
         self.model = model
         
     
+    @property
+    def shaper(self):
+        return self._shaper
     
+    @shaper.setter
+    def shaper(self, shaper):
+        if isinstance(shaper, str) and 1 <= len(shaper) <= 15:
+            self._shaper = shaper
+        else:
+            raise Exception('Shaper must be a string between 1 and 15 characters you barney!')
+        
+    @property
+    def size(self):
+        return self._size 
+    
+    @size.setter
+    def size(self, size):
+        if isinstance(size, int) and 4 <= size <= 14:
+            self._size = size
+        else:
+            raise Exception("Size of your board must be a number between 4 and 14...otherwise its not a surfboard")
+        
+    @property
+    def model(self):
+        return self._model
+    
+    @model.setter
+    def model(self, model):
+        if isinstance(model, str) and 1 <= len(model) <= 15:
+            self._model = model
+        else:
+            raise Exception('Model must be a string between 1 and 15 characters haole')
+        
+        
