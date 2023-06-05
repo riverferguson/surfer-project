5908e002228d374efff704bf85faaf6953cdb7e7
River Ferguson <riverferguson@gmail.com>
2023-06-04 19:28:13 -0700

    set up beaches class

---
+++ lib/classes/beaches.py
@@ -0,0 +1,9 @@
+from .__init__ import CONN, CURSOR
+
+class Beaches:
+    
+    def __init__(self, name, location, popularity):
+        self.name = name
+        self.location = location
+        self.popularity = popularity
+        
