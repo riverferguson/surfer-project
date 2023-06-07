dc7652c1d7ab170810e02f0c2bf0d51a872f51cd
River Ferguson <riverferguson@gmail.com>
2023-06-07 10:37:38 -0700

    continue creating helper functions for cli

--- lib/cli.py
+++ lib/cli.py
@@ -7,9 +7,16 @@ from helpers import (
     list_waves,
     find_beach_by_name,
     find_beach_by_name,
-    exit_program,
     find_difficulty_by_id,
-    find_motto
+    find_motto,
+    add_new_beach,
+    add_new_surfer,
+    add_new_surfboard,
+    find_most_dangerous_wave,
+    find_safest_wave,
+    find_most_popular_beach,
+    find_lest_popular_beach,
+    exit_program
 )
 
 def main():
@@ -34,7 +41,22 @@ def main():
         elif choice == '7':
             surfer_name = input('Enter surfer name to find motto: ')
             find_motto(surfer_name)
-        elif choice == '20':
+        elif choice == '8':
+            new_beach = input('Enter new beach: ')
+            add_new_beach(new_beach)
+        elif choice == '9':
+            pass
+        elif choice == '10':
+            pass
+        elif choice == '11':
+            find_most_dangerous_wave()
+        elif choice == '12':
+            find_safest_wave()
+        elif choice == '13':
+            find_most_popular_beach()
+        elif choice == '14':
+            find_lest_popular_beach()
+        elif choice == '15':
             exit_program
             break
         else:
--- lib/helpers.py
+++ lib/helpers.py
@@ -1,4 +1,5 @@
-import re 
+
+from rich import print 
 
 def welcome():
     print('Welcome to Wave-Tracker')
@@ -13,19 +14,14 @@ def menu():
     print('5. Find a beach by name')
     print('6. Find a difficulty of a wave by id')
     print('7. Find a surfer\'s motto by name')
-    print('8. Find a surfboard by id')
-    print('9. Add a new beach to our list')
+    print('8. Add a new beach to our list')
+    print('9. Add a new surfer')
     print('10. Add a new surfboard')
-    print('11. Add a new surfer')
-    print('12. Add a new wave')
-    print('13. Delete a broken surfboard')
-    print('14. Delete a tired surfer')
-    print('15. Delete a wave that is too dangerous')
-    print('16. Find most dangerous wave')
-    print('17. Find safest wave')
-    print('18. Find most popular beach')
-    print('19. Find least popular beach')
-    print('20. Exit')
+    print('11. Find most dangerous wave')
+    print('12. Find safest wave')
+    print('13. Find most popular beach')
+    print('14. Find least popular beach')
+    print('15. Exit')
     
 def list_beaches():
     beaches = Beach.find_all()
@@ -49,7 +45,11 @@ def list_surfboards():
         
 def find_beach_by_name(beach):
     beach_name = Beach.find_by_name(beach)
-    print(beach_name.location)
+    
+    if beach_name:
+        print(beach_name.location)
+    else:
+        raise Exception("Beach not found. Enter valid name")
     
 def find_difficulty_by_id(wave_id):
     difficulty_id = Wave.find_by_id(wave_id)
@@ -58,7 +58,54 @@ def find_difficulty_by_id(wave_id):
 def find_motto(first_name):
     motto = Surfer.find_by_name(first_name)
     print(motto.motto)
-            
+    
+def add_new_beach():
+    name = input("Enter new beach name: ")
+    location = input("Enter location of new beach: ")
+    popularity = input("Enter the popularity level (number between 1 & 10): ")
+    wave_id = input("Enter a wave ID: ")
+    surfer_id = input("Enter your surfer's ID: ")
+    if (
+        isinstance(name, str)
+    and isinstance(location, str)
+    and isinstance(popularity, int)
+    and isinstance(wave_id, int)
+    and isinstance(surfer_id, int)
+    and len(name)
+    and len(location)
+    and popularity
+    and wave_id
+    and surfer_id
+    ):
+        try:
+            new_beach = Beach.create(name, location, popularity, wave_id, surfer_id)
+            print(new_beach)
+        except Exception as error:
+            print("Error creating beach: ", error)
+    else:
+        print("Invalid name, location, popularity, wave_id, or surfer_id")
+        
+def add_new_surfer():
+    pass
+
+def add_new_surfboard():
+    pass
+    
+def find_most_dangerous_wave():
+    most_dangerous = Wave.find_most_dangerous()
+    print(most_dangerous.danger_level)
+
+def find_safest_wave():
+    safest_wave = Wave.find_safest_wave()
+    print(safest_wave.danger_level)
+
+def find_most_popular_beach():
+    most_popular = Beach.find_most_popular()
+    print(most_popular.name)
+
+def find_lest_popular_beach():
+    least_popular = Beach.find_least_popular()
+    print(least_popular.name)            
         
 def exit_program():
     print("Goodbye!")
